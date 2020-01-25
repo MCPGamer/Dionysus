7e29c1075afa7dfbe9c4ce7119858b69d7f15ed9
Pacmens <Magnesium13?>
2020-01-25 21:12:12 +0100

    fock this shit im out

--- dionysus/src/main/java/ch/duartemendes/dionysus/controller/DataTransferController.java
+++ dionysus/src/main/java/ch/duartemendes/dionysus/controller/DataTransferController.java
@@ -1,8 +1,11 @@
 package ch.duartemendes.dionysus.controller;
 
+import ch.duartemendes.dionysus.model.XMLContent;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import ch.duartemendes.dionysus.model.ApiHandler;
 import ch.duartemendes.dionysus.model.MediaService;
@@ -19,7 +22,28 @@ public class DataTransferController {
 	private ApiHandler apiHandler;
 
 	@GetMapping("dataTransfer")
-	private String getDataTransferRoot() {
+	private String getDataTransferRoot(Model model) {
+		model.addAttribute("xmlContent", new XMLContent());
+		return "dataTransfer.html";
+	}
+
+	@PostMapping("xmlUpload")
+	private String uploadXML(@ModelAttribute XMLContent xmlContent, Model model) {
+		if (xmlContent.getXml().contains("noNamespaceSchemaLocation=\"DataTransfer.xsd\"")) {
+			mediaService.importFromXml(xmlContent.getXml());
+			return "redirect:/";
+		} else {
+			model.addAttribute("xmlerror", "doesnt have dataTransfer.xsd");
+			model.addAttribute("xml", "");
+			return "dataTransfer.html";
+		}
+	}
+
+	@PostMapping("xmlExport")
+	public String exportXml(Model model) {
+		XMLContent xmlContent = new XMLContent();
+		xmlContent.setXml(mediaService.exportToXml());
+		model.addAttribute("xmlContent", xmlContent);
 		return "dataTransfer.html";
 	}
 }
---
+++ dionysus/src/main/java/ch/duartemendes/dionysus/model/XMLContent.java
@@ -0,0 +1,14 @@
+package ch.duartemendes.dionysus.model;
+
+public class XMLContent {
+    private String xml = "";
+
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(String xml) {
+        this.xml = xml;
+    }
+
+}
